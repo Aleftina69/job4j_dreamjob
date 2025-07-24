eaabe87e59e1f05f967d062e18e5df621fc04107
Brux <okladno94@gmail.com>
2025-07-24 15:37:40 +0500

    1. Генератор HTML [#504838]

--- job4j_dreamjob-master/src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ job4j_dreamjob-master/src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -2,14 +2,12 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@SuppressWarnings("unused")
 public class IndexController {
+
     @GetMapping("/index")
-    @ResponseBody
     public String getIndex() {
-        return "Hello World!";
+        return "index";
     }
 }
