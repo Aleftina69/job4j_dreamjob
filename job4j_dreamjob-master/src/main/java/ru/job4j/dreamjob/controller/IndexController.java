d3be61eca621cf672db34739d60bdba20575c566
Brux <okladno94@gmail.com>
2025-07-24 14:11:38 +0500

    Реализован "Hello World!" Обзор клиент - серверное приложение [#504837]

--- job4j_dreamjob-master/src/main/java/ru/job4j/dreamjob/controller/IndexController.java
+++ job4j_dreamjob-master/src/main/java/ru/job4j/dreamjob/controller/IndexController.java
@@ -2,12 +2,14 @@ package ru.job4j.dreamjob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SuppressWarnings("unused")
 public class IndexController {
     @GetMapping("/index")
+    @ResponseBody
     public String getIndex() {
-        return "index";
+        return "Hello World!";
     }
 }
